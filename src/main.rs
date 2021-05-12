c60a0c83c95ba415b26d9d3215c5f761e1244189
Анатолий <workmail018@gmail.com>
2021-05-12 12:22:24 +0300

    init

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use std::io;
+fn main() {
+    println!("{:?}", to_bytes());
+}
+
+fn to_bytes() -> Vec<u8>{
+    let mut raw_string: String = String::new();
+
+    io::stdin().read_line(&mut raw_string).expect("Ups...");
+
+    let bytes_string:Vec<u8> = raw_string.into_bytes();
+    
+    bytes_string
+}
