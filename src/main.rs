d9ae37fa4c8bacde9fe2a7d00dbf8d7f637aeb35
Анатолий <workmail018@gmail.com>
2021-05-12 12:44:14 +0300

    add println!

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() {
 fn to_bytes() -> Vec<u8>{
     let mut raw_string: String = String::new();
 
+    println!("Enter characters ");
     io::stdin().read_line(&mut raw_string).expect("Ups...");
 
     let bytes_string:Vec<u8> = raw_string.into_bytes();
